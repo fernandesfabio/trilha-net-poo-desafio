9420809acf75a0d3f5ead40bc7f572a3206bd37d
fernandesfabio <fabiosbcity@icloud.com>
2024-09-28 19:57:45 -0300

    Feito a resolução do desáfio proposto pela DIO.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+
+Smartphone nokia = new Nokia(numero: "123456", modelo: "modelo 1", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+
+Smartphone iphone = new Iphone(numero: "1234567",modelo: "Modelo 2", imei: "x12uh34", memoria: 32);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
